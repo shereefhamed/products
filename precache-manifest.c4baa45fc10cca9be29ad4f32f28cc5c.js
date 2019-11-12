1e94aafb8d2cdf386a1058dbacef4d9f1d815383
shereefhamed <47435746+shereefhamed@users.noreply.github.com>
2019-11-12 22:40:48 +0200

    Add files via upload

---
+++ precache-manifest.c4baa45fc10cca9be29ad4f32f28cc5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a02fe092004f5697ccafd4392fe7ee",
+    "url": "/products/index.html"
+  },
+  {
+    "revision": "1381b3624d1ae9fa48b2",
+    "url": "/products/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "1381b3624d1ae9fa48b2",
+    "url": "/products/static/js/2.86bbb493.chunk.js"
+  },
+  {
+    "revision": "afb3f99926984b22973e",
+    "url": "/products/static/js/main.3a25ce79.chunk.js"
+  },
+  {
+    "revision": "e3f95eca52c329d6fe7a",
+    "url": "/products/static/js/runtime-main.063870b0.js"
+  }
+]);
